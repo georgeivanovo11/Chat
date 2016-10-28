ba93783a198018ae82637845ce08bc9048cc396f
georgeivanovo11 <george-mamardashvili@yandex.ru>
2016-10-28 22:42:59 +0300

    Project starts. NavigationController creates. LoginController is clean.

---
+++ Chat/Chat/Controllers/LoginController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginController.swift
+//  Chat
+//
+//  Created by george on 28/10/2016.
+//  Copyright © 2016 george. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+}
