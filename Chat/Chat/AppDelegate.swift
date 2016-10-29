e3a28a1facc9e84c54dc103f563fd0a81c1e2c2d
georgeivanovo11 <george-mamardashvili@yandex.ru>
2016-10-29 17:08:13 +0300

    Firebase SDK installed

--- Chat/Chat/AppDelegate.swift
+++ Chat/Chat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
+        FIRApp.configure()
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
